d5141be29771ab858ffad3af4af69edf5a25ce33
Viacheslav Panasiuk <badgerle82@gmail.com>
2022-12-08 11:17:01 +0200

    Build.gradle dependencies were added

--- app/src/main/java/com/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/stockmarketapp/MainActivity.kt
@@ -3,6 +3,10 @@ package com.stockmarketapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
 import com.stockmarketapp.ui.theme.StockMarketAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +14,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             StockMarketAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
 
+                }
             }
         }
     }
